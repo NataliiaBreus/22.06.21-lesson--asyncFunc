d6af52cb2497f06e2e604a0a5d336047cc519bd5
Nataliia Breus <breusnataly@gmail.com>
2021-06-10 12:21:21 +0300

    changes

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import style from './Home.module.sass'
-
+import React, {useContext} from 'react';
+import style from './Home.module.sass';
+import cx from 'classnames';
+import CONSTANTS from '../../constants';
+const {THEMES} = CONSTANTS;
+import {ThemeContext} from '../../contexts';
 
 /*
   1. Переделать переключение тем на хуки
   2. Добавить переключение темы в Home
 */
 function Home () {
-  return <div className={style.container}>HOME PAGE</div>;
+  const [theme] = useContext(ThemeContext);
+  const classes = cx(style.container, {
+    [style.lightTheme]: theme === THEMES.LIGHT,
+    [style.darkTheme]: theme === THEMES.DARK,
+  });
+  return <div className={classes}>HOME PAGE</div>;
 }
 
 export default Home;
